c03e8d6471caabbbc3cbd8342d0519db5c2f6eb7
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-21 15:06:57 +0100

    remove routing code

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import routes from './server/routes';
+
 
 const app = express();
 const http = require('http');
@@ -10,26 +12,7 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// An API route that allow users create broadcast groups:
-// POST: /api/group
-app.post('/api/group', (req, res) => res.send('Create a group'));
-
-
-// An API route that allow users add other users to groups:
-app.post('/api/group/:groupid/user', (req, res) => res.send(`Added new user to ${req.params.groupid}.`));
-
-
-// An API route that allows a logged in user post messages to created groups:
-app.post('/api/group/:groupid/message', (req, res) => res.send(`Thanks for posting ur message to ${req.params.groupid}`));
-
-// An API route that allows a logged in user retrieve messages that have been
-// posted to groups he/she belongs to:
-app.get('/api/group/:groupid/messages', (req, res) => res.send(`Welcome! here are the messages for ${req.params.groupid}`));
-
-// Root route
-app.get('*', (req, res) => res.send('Sorry, the page u requested does not exist'));
-
+routes(app);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('The server is running on port 8080'));
