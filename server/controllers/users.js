ea75d84963a2523cc7264045519e1cd2fe804c57
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-21 14:38:04 +0100

    edit code for username, password and email attributes

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,9 @@ module.exports = {
   create(req, res) {
     return User
       .create({
-        title: req.body.title,
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
       })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
