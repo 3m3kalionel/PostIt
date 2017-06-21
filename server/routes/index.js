9f3cb2d422170251fab2e3e5b977a91bba15ffc9
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-21 15:08:02 +0100

    encapsulate routing code

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,22 @@ module.exports = (app) => {
   }));
 
   app.post('/api/user/signup', usersController.create);
+
+  // An API route that allow users create broadcast groups:
+  // POST: /api/group
+  app.post('/api/group', (req, res) => res.send('Create a group'));
+
+  // An API route that allow users add other users to groups:
+  app.post('/api/group/:groupid/user', (req, res) => res.send(`Added new user to ${req.params.groupid}.`));
+
+
+  // An API route that allows a logged in user post messages to created groups:
+  app.post('/api/group/:groupid/message', (req, res) => res.send(`Thanks for posting ur message to ${req.params.groupid}`));
+
+  // An API route that allows a logged in user retrieve messages that have been
+  // posted to groups he/she belongs to:
+  app.get('/api/group/:groupid/messages', (req, res) => res.send(`Welcome! here are the messages for ${req.params.groupid}`));
+
+  // Root route
+  app.get('*', (req, res) => res.send('Sorry, the page u requested does not exist'));
 };
