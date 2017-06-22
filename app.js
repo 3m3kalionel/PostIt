5afe2ceed4688546ebd00680faad20eb369a2e23
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-22 17:06:43 +0100

    add passport middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
-import routes from '../server/routes';
+import passport from 'passport';
+import routes from './server/routes';
+
 
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 routes(app);
 
 const server = http.createServer(app);
