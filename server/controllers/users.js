d34a2928eec1f8466f3c45236ab9a6e91b39379c
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-22 17:44:28 +0100

    Fix bugs in user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,13 +2,15 @@ const User = require('../models').User;
 
 module.exports = {
   create(req, res) {
-    return User
-      .create({
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email,
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
+    User.sync({ force: false }).then(function() {
+      return User
+        .create({
+          username: req.body.username,
+          password: req.body.password,
+          email: req.body.email,
+        })
+        .then(user => res.status(201).send(user))
+        .catch(error => res.status(400).send(error));
+    });
+  }
 };
