fdd9b17ed38fdd2d5628210ca62954f320841846
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-22 17:27:09 +0100

    refactor code using es6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,19 +4,8 @@ import nodemon from 'gulp-nodemon';
 
 gulp.task('start', () => {
   nodemon({
-    script: 'dest/app.js',
+    script: 'index.js',
     env: { NODE_ENV: 'development' }
   });
 });
 
-gulp.task('runServer', () => {
-  return gulp.src('app.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dest'));
-});
-
-gulp.task('watch', () => {
-  gulp.watch('app.js', ['runServer']);
-});
-
-gulp.task('default', ['start', 'runServer', 'watch']);
