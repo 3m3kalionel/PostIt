a12280ec4c932006088634573a399f3b9f9707d4
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-23 02:00:05 +0100

    Refactor using es6 syntax

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -2,10 +2,10 @@ import passport from './auth';
 
 module.exports = {
   login(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
+    passport.authenticate('local', (err, user, info) => {
       if (err) { return next(err); }
-      if (!user) { return res.status(400).send(user) }
-      req.logIn(user, function(err) {
+      if (!user) { return res.status(400).send(user); }
+      req.logIn(user, (err) => {
         if (err) { return next(err); }
         return res.status(200).send('logged in');
       });
