49427bf90b9a0d00515c0c91e9aaf7848952fd51
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-23 02:13:45 +0100

    remove eslint flagged errors

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -2,7 +2,7 @@ import passport from './auth';
 
 module.exports = {
   login(req, res, next) {
-    passport.authenticate('local', (err, user, info) => {
+    passport.authenticate('local', (err, user) => {
       if (err) { return next(err); }
       if (!user) { return res.status(400).send(user); }
       req.logIn(user, (err) => {
