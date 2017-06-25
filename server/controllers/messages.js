71347f8c8b62ed158055d7ac1897435808f59f68
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-25 15:11:05 +0100

    Refactor using es6 syntax

---
+++ server/controllers/messages.js
@@ -0,0 +1,16 @@
+import models from '../models';
+
+const Message = models.Message;
+
+module.exports = {
+  create(req, res) {
+    Message.sync({ force: false }).then(() => {
+      Message
+        .create({
+          content: req.body.content,
+        })
+        .then(text => res.status(201).send(text))
+        .catch(error => res.status(400).send(error));
+    });
+  }
+};
