9d0b8fc606927edbd610b483d1007bd6bef59672
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-26 01:25:19 +0100

    Bug fixes for sign in password validation

--- server/helpers/validate.js
+++ server/helpers/validate.js
@@ -4,6 +4,8 @@ import encryption from './encrypt';
 module.exports = {
   validatePassword(dbPassword, reqPassword) {
     const passwordHash = encryption.encryptPassword(reqPassword);
+    console.log(dbPassword);//
+    console.log(passwordHash);//
     return bcrypt.compareSync(dbPassword, passwordHash);
   }
 };
