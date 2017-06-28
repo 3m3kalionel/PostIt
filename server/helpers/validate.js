667c1facdf822e3dd8e3960065172dde74b0a870
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-28 12:56:56 +0100

    Re-write function to fix validation issue

--- server/helpers/validate.js
+++ server/helpers/validate.js
@@ -2,10 +2,15 @@ import bcrypt from 'bcrypt';
 import encryption from './encrypt';
 
 module.exports = {
+  // validatePassword(dbPassword, reqPassword) {
+  //   const passwordHash = encryption.encryptPassword(reqPassword);
+  //   console.log(dbPassword);//
+  //   console.log(passwordHash);//
+  //   return bcrypt.compareSync(dbPassword, passwordHash);
+  // }
+
   validatePassword(dbPassword, reqPassword) {
-    const passwordHash = encryption.encryptPassword(reqPassword);
-    console.log(dbPassword);//
-    console.log(passwordHash);//
-    return bcrypt.compareSync(dbPassword, passwordHash);
+    const encrypt = bcrypt.hashSync(reqPassword, encryption.encryptPassword(reqPassword).salt);
+    return bcrypt.compareSync(dbPassword, encrypt);
   }
 };
