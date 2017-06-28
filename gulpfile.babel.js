d1e3d8f113284ccf3b4daf1eba579e92f20170f9
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-28 17:59:46 +0100

    Add files for coverage testing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,9 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import nodemon from 'gulp-nodemon';
+import mocha from 'gulp-mocha';
+import istanbul from 'gulp-babel-istanbul';
+import injectModules from 'gulp-inject-modules';
 
 gulp.task('start', () => {
   nodemon({
@@ -9,3 +12,18 @@ gulp.task('start', () => {
   });
 });
 
+
+gulp.task('coverage', function (cb) {
+  gulp.src('src/**/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', () => {
+      gulp.src('test/**/*.js')
+        .pipe(babel())
+        .pipe(injectModules())
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+        .on('end');
+    });
+});
