b6e410c7497bdea126059c4dc3388b90da5ab8f4
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-29 00:24:10 +0100

    Add test for signup route

--- test/postIt.test.js
+++ test/postIt.test.js
@@ -4,3 +4,25 @@ import app from '../app';
 
 const expect = chai.expect;
 
+const user1 = {
+  username: 'dj',
+  password: '12345',
+  email: 'mighty.mouse@ufc.com'
+};
+
+describe('signup route', () => {
+  it('should create a new user', (done) => {
+    request(app).post('/api/user/signup')
+      .send(user1)
+      .expect((res) => {
+        expect(res.body).to.be.an.instanceof(Object);
+        expect(201);
+        expect(res.body.username).to.equal(user1.username);
+        expect(res.body.email).to.equal(user1.email);        
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+        done();
+      });
+  });
+});
