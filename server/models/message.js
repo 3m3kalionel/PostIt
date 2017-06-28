d3199ef1a17186b344c32ac457fe3b3f1476f6e2
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-28 13:55:11 +0100

    Edit foreignKey value

--- server/models/message.js
+++ server/models/message.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
           foreignKey: 'groupId',
           onDelete: 'CASCADE',
         },
-        Message.belongsTo(models.Group, {
+        Message.belongsTo(models.User, {
           foreignKey: 'userId',
           onDelete: 'CASCADE',
         })
