9f7f95b2436210969a436cb3ed82b3380a044c46
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-29 12:42:23 +0100

    Edit status code for invalid sign in username

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -4,7 +4,7 @@ module.exports = {
   login(req, res, next) {
     passport.authenticate('local', (err, user) => {
       if (err) { return next(err); }
-      if (!user) { return res.status(400).send(user); }
+      if (!user) { return res.status(404).send(user); }
       req.logIn(user, (err) => {
         if (err) { return next(err); }
         return res.status(200).send('logged in');
