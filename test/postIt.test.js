db745e2385213f942f5e170cbc1670feaad23307
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-06-29 14:07:45 +0100

    Add tests for unregistered user

--- test/postIt.test.js
+++ test/postIt.test.js
@@ -11,6 +11,12 @@ const user1 = {
   email: 'mighty.mouse@ufc.com'
 };
 
+const unregisteredUser = {
+  usename: 'falseUser',
+  password: '12345',
+  email: 'unsignedUser@gmail.com'
+}
+
 describe('signup route', () => {
   beforeEach((done) => {
     models.User.destroy({
@@ -35,4 +41,43 @@ describe('signup route', () => {
         done();
       });
   });
+});
+
+
+describe('signin route', () => {
+  it('should sign in an existing user', (done) => {
+    request(app).post('/api/user/signin')
+      .send(user1)
+      .expect((res) => {
+        // expect(res.body).to.be.an.instanceof(Object);
+        expect(200);
+        // expect(res.body.username).to.equal(user1.username);
+        expect(res.text).to.equal('logged in');
+      })
+      .end((err, res) => {
+        if (err) { 
+          // console.log(res);
+          return done(err); }
+        done();
+      });
+  });
+
+  it('should reject an unregistered user', (done) => {
+    request(app).post('/api/user/signin')
+      .send(unregisteredUser)
+      .expect((res) => {
+        // expect(res.body).to.be.an.instanceof(Object);
+        expect(404);
+        // expect(res.body.username).to.equal(user1.username);
+        expect(res.text).to.equal('false');
+      })
+      .end((err, res) => {
+        if (err) { 
+          // console.log(res);
+          return done(err); }
+        done();
+      });
+  });
+
+  
 });
