d932a396019d6c145b4bf774ac7739ccadff4e0a
Emeka Okoro <emeka.l.okoro@gmail.com>
2017-07-01 10:25:19 +0100

    Group creation Bug fixes

--- server/controllers/groups.js
+++ server/controllers/groups.js
@@ -10,15 +10,22 @@ User.belongsToMany(Group, { through: 'UserGroup' });
 
 module.exports = {
   create(req, res) {
-    Group.sync({ force: false }).then(() => {
-      Group
-        .create({
-          name: req.body.name,
-          description: req.body.description
-        })
-        .then(groups => res.status(201).send(groups))
-        .catch(error => res.status(400).send(error));
-    });
+    models.User.find({ where: { email: req.body.email } }).then((user) => {
+      Group.sync({ force: false }).then(() => {
+        Group
+          .create({
+            name: req.body.name,
+            description: req.body.description
+          }).then((group) => {
+            group.addUser(user)
+              .then(() => { 
+                if (!user) {
+                  res.send('user not found');
+                } else { return res.send('Hello'); } });
+          })
+          .catch((error) => { res.status(400).send(error); });
+      });
+    }).catch((error) => res.send(error));
   },
   list(req, res) {
     const groupid = req.params.groupid;
