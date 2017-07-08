5985ca93a98faf9ac7e3199426a982befe1a45af
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-08 15:35:09 +0100

    Refactor code

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,20 +6,30 @@ const User = user.User;
 
 module.exports = {
   create(req, res) {
+    const username = req.body.username;
+    const email = req.body.email;
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    User.sync({ force: false }).then(() => {
-      User
-        .create({
-          username: req.body.username,
-          password: hash,
-          email: req.body.email,
-          salt: salt
-        })
-        .then(newUser => res.status(201).send(newUser))
-        .catch(error => res.status(400).send(error));
+    User.find({
+      where: {
+        username
+      }
+    }).then(() => {
+      User.sync({ force: false }).then(() => {
+        User
+          .create({
+            username,
+            password: hash,
+            email,
+            salt
+          })
+          .then(newUser => res.status(201).send(newUser))
+          .catch(error => res.status(400).send(error));
+      });
     });
+
+    
   }
 };
 
