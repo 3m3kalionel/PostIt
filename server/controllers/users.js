5a8ff7967943e36a4b833e55ead61a6c9462473e
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-08 15:21:09 +0100

    Delete commented code

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -24,23 +24,3 @@ module.exports = {
   }
 };
 
-// import helpers from '../helpers';
-// import user from '../models';
-
-// const User = user.User;
-
-// module.exports = {
-//   create(req, res) {
-//     User.sync({ force: false }).then(() => {
-//       User
-//         .create({
-//           username: req.body.username,
-//           password: helpers.encrypt.encryptPassword(req.body.password).hash,
-//           email: req.body.email,
-//           salt: helpers.encrypt.encryptPassword(req.body.password).salt
-//         })
-//         .then(user => res.status(201).send(user))
-//         .catch(error => res.status(400).send(error));
-//     });
-//   }
-// };
