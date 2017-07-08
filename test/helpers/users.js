dd2b69e01f921b7613f87ab6a1e9ffaf6ce80528
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-08 21:13:03 +0100

    Add dummy test data

--- test/helpers/users.js
+++ test/helpers/users.js
@@ -1,7 +1,21 @@
 module.exports = {
   validUser: {
-    userName: 'conor',
+    username: 'ibrahim',
     password: '12345678',
-    email: 'conor@andela.com',
+    email: 'ibrahim@andela.com',
   },
+  nullUsername: {
+    password: '12345678',
+    email: 'abc@andela.com'
+  },
+  notUniqueEmail: {
+    username: 'conor',
+    password: '12345678',
+    email: 'ibrahim@andela.com',
+  },
+  nullEmail: {
+    username: 'jonjones',
+    password: '12345678'
+  }
+
 };
