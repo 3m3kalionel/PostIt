42e0bed20120ae9576d674f4fd07b6b01b0a1d4d
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-09 12:19:18 +0100

    Remove redundant code

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,8 +1,7 @@
-import helpers from '../helpers';
-import user from '../models';
 import bcrypt from 'bcrypt';
+import model from '../models';
 
-const User = user.User;
+const User = model.User;
 
 module.exports = {
   create(req, res) {
@@ -14,30 +13,24 @@ module.exports = {
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     if (!validator.test(password)) {
-      return res.status(400).send('Your password length should be between EIGHT and TWENTY characters');
+      return res.status(400).json({
+        signupError: 'Your password length should be between EIGHT and TWENTY characters'
+      });
     }
 
-    User.find({
-      where: {
-        username
-      }
-    }).then(() => {
-      User.sync({ force: false }).then(() => {
-        User
-          .create({
-            username,
-            password: hash,
-            email,
-            salt
-          })
-          .then(newUser => res.status(201).send(newUser))
-          .catch((error) => {
-            res.status(400).json({
-              loginError: error.errors[0].message
-            });
-          });
-      }).catch(error => res.status(400).send(error.name));
-    });
+    User
+      .create({
+        username,
+        password: hash,
+        email,
+        salt
+      })
+      .then(newUser => res.status(201).send(newUser))
+      .catch((error) => {
+        res.status(400).json({
+          loginError: error.errors[0].message
+        });
+      });
   }
 };
 
