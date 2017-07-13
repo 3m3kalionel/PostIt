c081f82c49b21aeefdc03cee0c68cd3a23a17952
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-13 14:55:41 +0100

    Refactor jwt implementation

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,4 +1,8 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import passport from './auth';
+
+dotenv.config();
 
 module.exports = {
   login(req, res, next) {
@@ -7,6 +11,18 @@ module.exports = {
       if (!user) { return res.status(404).send(user); }
       req.logIn(user, (err) => {
         if (err) { return next(err); }
+        const token = jwt.sign({
+          username: user.username,
+          email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '2 days'
+        });
+        res.status(201).json({
+          user,
+          tok: token
+        });
         return res.status(200).send('logged in');
       });
     })(req, res, next);
