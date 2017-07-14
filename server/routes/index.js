4f2f1a58de6811086fec4ade8a2f0fad9aae8fa2
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-14 04:12:01 +0100

    Seperate concerns - remove middleware logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import authControllers from '../controllers';
+import authenticate from '../middleware/authenticate';
 
 const usersController = authControllers.users;
 const loginController = authControllers.login;
@@ -18,45 +18,20 @@ module.exports = (app) => {
 
   app.post('/api/user/signup', usersController.create);
 
-  app.use((req, res, next) => {
-    // check header or url parameters or post parameters for token
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
-
-    // decode token
-    if (token) {
-      // verifies secret and checks exp
-      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-          return res.json({ success: false, message: 'Failed to authenticate token.' });
-        }
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;
-        next();
-      });
-    } else {
-      // if there is no token
-      // return an error
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
-    }
-  });
-
   // An API route that allow users create broadcast groups:
   // POST: /api/group
-  app.post('/api/group', groupsController.create);
+  app.post('/api/group', authenticate, groupsController.create);
 
   // An API route that allow users add other users to groups:
-  app.post('/api/group/:groupid/user', groupsController.addNewUser);
+  app.post('/api/group/:groupid/user', authenticate, groupsController.addNewUser);
 
 
   // An API route that allows a logged in user post messages to created groups:
-  app.post('/api/group/:groupid/message', messagesController.create);
+  app.post('/api/group/:groupid/message', authenticate, messagesController.create);
 
   // An API route that allows a logged in user retrieve messages that have been
   // posted to groups he/she belongs to:
-  app.get('/api/group/:groupid/messages', groupsController.list);
+  app.get('/api/group/:groupid/messages', authenticate, groupsController.list);
 
   // Root route
   app.get('*', (req, res) => res.send('Sorry, the page u requested does not exist'));
