ea4187aa868bf240e63339b0db566788686d62e3
3m3kalionel <emeka.l.okoro@gmail.com>
2017-07-31 00:19:06 +0100

    Add configuration settings for webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,57 @@
 import path from 'path';
+import webpack from 'webpack';
 
 const PUBLIC_PATH = path.join(__dirname, 'public');
 const SRC_DIRECTORY = path.join(__dirname, 'client');
 
 
 export default {
-  entry: path.join(SRC_DIRECTORY, 'index.js'),
+  entry: [
+    'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
+    path.join(SRC_DIRECTORY, 'index.jsx'),
+  ],
   output: {
     path: PUBLIC_PATH,
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loaders: [
-          'babel-loader'
-        ]
+        include: [
+          path.resolve(__dirname, 'client')
+        ],
+        exclude: /node_modules/,
+        use: ['react-hot-loader', 'babel-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|styl|jpe?g|png|gif|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.scss?$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false
+            }
+          },
+        ],
       }
-    ]
+    ],
   },
   resolve: {
-    extensions: ['.js', '.json', '.jsx', '.css']
-  }
+    extensions: ['.js', '.json', '.jsx', '.scss', '.css'],
+  },
+
 };
