27912a421479d2c4216169873e79a09267792f00
3m3kalionel <emeka.l.okoro@gmail.com>
2017-08-25 09:16:07 +0100

    Edit logic for  creating mesage success action

--- client/reducers/groupReducer.js
+++ client/reducers/groupReducer.js
@@ -30,20 +30,20 @@ const groupReducer = (state = {}, action = {}) => {
         }
       };
     case message.CREATE_SUCCESS:
-      return { ...state,
+      // return { ...state,
+      //   [action.message.groupId]: {
+      //     messages: [...state[action.message.groupId].messages, action.message]
+      //   } };
+      return {
+        ...state,
         [action.message.groupId]: {
-          messages: [...state[action.message.groupId].messages, action.message]
-        } };
-      // return {
-      //   ...state,
-      //   [action.groupId]: {
-      //     ...state[action.groupId],
-      //     messages: [
-      //       ...state[action.groupId].messages,
-      //       action.message
-      //     ]
-      //   }
-      // };
+          ...state[action.message.groupId],
+          messages: [
+            ...state[action.message.groupId].messages,
+            action.message
+          ]
+        }
+      };
     case message.LIST_SUCCESS:
       return {
         ...state,
