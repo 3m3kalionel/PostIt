70e1ec63a95e4a67f03e8f6f05ed155761f78511
3m3kalionel <emeka.l.okoro@gmail.com>
2017-08-25 08:57:18 +0100

    Search for users using a query string

--- server/routes/index.js
+++ server/routes/index.js
@@ -43,7 +43,7 @@ module.exports = (app) => {
   app.get('/api/group/:groupid/users', authenticate, validateGroup.validGroup, validateGroup.isGroupMember, groupsController.listMembers);
 
   // An API route that allows a logged in user search for a user
-  app.post('/api/users', usersController.listAll);
+  app.get('/api/users', usersController.listAll);
 
   // An API route that allows a logged in user list all groups that he/she belongs to
   app.get('/api/groups', authenticate, groupsController.listGroups);
